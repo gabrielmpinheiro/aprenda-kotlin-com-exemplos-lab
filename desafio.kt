e66ff9639cceca06f58f6938999fa6393ec5ee6d
Gabriel Pinheiro <gabrielmpinheiro2@gmail.com>
2023-09-10 10:10:50 -0300

    Cadastrando aluno, Efetuando matrícula

--- desafio.kt
+++ desafio.kt
@@ -11,14 +11,41 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
     val inscritos = mutableListOf<Aluno>()
 
     fun matricular(usuario: Aluno) {
-        //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        //add ao final de inscritos
+        inscritos.add(usuario)
+    }
+
+    fun getInscritos(){
+        println(inscritos)
     }
 }
 
-class Aluno(var nome: String) : Usuario()
+data class Aluno(var nome: String) : Usuario()
 
 fun main() {
+
+    var curso1 = ConteudoEducacional("PHP", 50)
+    var curso2 = ConteudoEducacional("JAVA", 70)
+    var curso3 = ConteudoEducacional("Spring", 40)
+
+    var conteudos = mutableListOf<ConteudoEducacional>()
+
+    conteudos.add(curso1)
+    conteudos.add(curso2)
+    conteudos.add(curso3)
+
+    var formacao1 = Formacao("Formação Webmaster", conteudos)
+
+    var aluno1 = Aluno("Gabriel")
+    var aluno2 = Aluno("Rafaela")
+    var aluno3 = Aluno("Dinda")
+    formacao1.matricular(aluno1)
+    formacao1.matricular(aluno2)
+    formacao1.matricular(aluno3)
+
     //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
     //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
-    println("oi")
+
+    println(formacao1.getInscritos())
+
 }
