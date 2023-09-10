f5efdef45046df5de3439786f4cd65ffb11ed46f
Gabriel Pinheiro <gabrielmpinheiro2@gmail.com>
2023-09-10 10:55:21 -0300

    Adicionando forma para descadastrar, impedindo o cadastro com nome repetido

--- desafio.kt
+++ desafio.kt
@@ -1,3 +1,5 @@
+import java.lang.Exception
+
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
@@ -9,14 +11,28 @@ data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Aluno>()
+    val report: List<Aluno> = inscritos
 
     fun matricular(usuario: Aluno) {
         //add ao final de inscritos
-        inscritos.add(usuario)
+        if (inscritos.contains(usuario)){
+            throw Exception("Usuário já está matriculado!")
+        } else {
+            inscritos.add(usuario)
+        }
+
+    }
+
+    fun desmatricular(usuario: Aluno){
+        //busca o aluno dentro de inscritos, e se encontrar, o remove
+        if (inscritos.contains(usuario)){
+            inscritos.remove(usuario)
+        }
     }
 
     fun getInscritos(){
-        println(inscritos)
+        println("Usuarios cadastrados na formação $nome :")
+        println(report)
     }
 }
 
@@ -28,24 +44,43 @@ fun main() {
     var curso2 = ConteudoEducacional("JAVA", 70)
     var curso3 = ConteudoEducacional("Spring", 40)
 
+    var curso4 = ConteudoEducacional("HTML", 50)
+    var curso5 = ConteudoEducacional("CSS", 70)
+    var curso6 = ConteudoEducacional("Bootstrap", 40)
+
     var conteudos = mutableListOf<ConteudoEducacional>()
 
     conteudos.add(curso1)
     conteudos.add(curso2)
     conteudos.add(curso3)
 
+    var novosConteudos = mutableListOf<ConteudoEducacional>()
+
+    novosConteudos.add(curso4)
+    novosConteudos.add(curso5)
+    novosConteudos.add(curso6)
+
     var formacao1 = Formacao("Formação Webmaster", conteudos)
+    var formacao2 = Formacao("Formacao Webdesigner", novosConteudos)
 
     var aluno1 = Aluno("Gabriel")
     var aluno2 = Aluno("Rafaela")
     var aluno3 = Aluno("Dinda")
+
     formacao1.matricular(aluno1)
     formacao1.matricular(aluno2)
     formacao1.matricular(aluno3)
 
-    //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    formacao2.matricular(aluno1)
 
     println(formacao1.getInscritos())
+    println(formacao2.getInscritos())
+
+    formacao1.desmatricular(aluno3)
+
+    println("=========================")
+    println(formacao1.getInscritos())
+
+    //formacao1.matricular(aluno1)
 
 }
