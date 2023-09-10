785babc929ba74fd57b5accf27634cc62f374094
Gabriel Pinheiro <gabrielmpinheiro2@gmail.com>
2023-09-10 11:37:15 -0300

    Add Nível para os alunos, Bloqueia matrícula em cursos AVANÇADOS caso o aluno não seja do mesmo nível

--- desafio.kt
+++ desafio.kt
@@ -2,13 +2,13 @@ import java.lang.Exception
 
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { INICIANTE, INTERMEDIARIO, AVANCADO }
 
 open class Usuario
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
     val inscritos = mutableListOf<Aluno>()
     val report: List<Aluno> = inscritos
@@ -17,10 +17,15 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
         //add ao final de inscritos
         if (inscritos.contains(usuario)){
             throw Exception("Usuário já está matriculado!")
-        } else {
-            inscritos.add(usuario)
         }
 
+        if (nivel.equals(Nivel.AVANCADO)) {
+            if(!usuario.nivel.equals(nivel)) {
+                throw Exception("Apenas usuários nível avançado podem fazer formações Avançadas")
+            }
+        }
+
+        inscritos.add(usuario)
     }
 
     fun desmatricular(usuario: Aluno){
@@ -36,51 +41,65 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
     }
 }
 
-data class Aluno(var nome: String) : Usuario()
+data class Aluno(var nome: String, var nivel: Nivel) : Usuario()
 
 fun main() {
 
+    /* Criando Cadastrando cursos */
     var curso1 = ConteudoEducacional("PHP", 50)
     var curso2 = ConteudoEducacional("JAVA", 70)
     var curso3 = ConteudoEducacional("Spring", 40)
-
     var curso4 = ConteudoEducacional("HTML", 50)
     var curso5 = ConteudoEducacional("CSS", 70)
     var curso6 = ConteudoEducacional("Bootstrap", 40)
+    var curso7 = ConteudoEducacional("Kotlin e Spring", 100)
+    var curso8 = ConteudoEducacional("MongoDB", 80)
+    var curso9 = ConteudoEducacional("Java Avançado", 120)
 
+    /* Criando e adicionando os cursos às listas*/
     var conteudos = mutableListOf<ConteudoEducacional>()
+    var novosConteudos = mutableListOf<ConteudoEducacional>()
+    var conteudosAvancados = mutableListOf<ConteudoEducacional>()
 
     conteudos.add(curso1)
     conteudos.add(curso2)
     conteudos.add(curso3)
-
-    var novosConteudos = mutableListOf<ConteudoEducacional>()
-
     novosConteudos.add(curso4)
     novosConteudos.add(curso5)
     novosConteudos.add(curso6)
+    conteudosAvancados.add(curso7)
+    conteudosAvancados.add(curso8)
+    conteudosAvancados.add(curso9)
 
-    var formacao1 = Formacao("Formação Webmaster", conteudos)
-    var formacao2 = Formacao("Formacao Webdesigner", novosConteudos)
+    /* Criando as formações com base nos conteúdos educacionais previamente criados */
+    var formacao1 = Formacao("Formação Webmaster", conteudos, Nivel.INTERMEDIARIO)
+    var formacao2 = Formacao("Formacao Webdesigner", novosConteudos, Nivel.INICIANTE)
+    var formacao3 = Formacao("Formação Avançada", conteudosAvancados, Nivel.AVANCADO)
 
-    var aluno1 = Aluno("Gabriel")
-    var aluno2 = Aluno("Rafaela")
-    var aluno3 = Aluno("Dinda")
+    /* Criando os alunos e cadastrando-os nos cursos*/
+    var aluno1 = Aluno("Gabriel", Nivel.AVANCADO)
+    var aluno2 = Aluno("Rafaela", Nivel.INTERMEDIARIO)
+    var aluno3 = Aluno("Dinda", Nivel.INICIANTE)
 
     formacao1.matricular(aluno1)
     formacao1.matricular(aluno2)
     formacao1.matricular(aluno3)
-
     formacao2.matricular(aluno1)
 
+    /* Pega todos os integrantes da primeira formação */
     println(formacao1.getInscritos())
+
+    /* Pega todos os integrantes da segunda formação*/
     println(formacao2.getInscritos())
 
+    /* Desmatricula um aluno de um curso */
     formacao1.desmatricular(aluno3)
 
-    println("=========================")
-    println(formacao1.getInscritos())
+    //A formação AVANCADO só vai permitir cadastrar alunos de nível avançado. Os demais aceitam alunos de qualquer nível.
+    //Provoca uma exception
+    formacao3.matricular(aluno3)
 
-    //formacao1.matricular(aluno1)
+    /* provoca uma Exception: Tenta cadastrar novamente um aluno já cadastrado na formação */
+    formacao1.matricular(aluno1)
 
 }
